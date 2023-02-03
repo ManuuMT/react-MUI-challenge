58def9afcf55426e96051cf361f153325eb8b400
Emanuel Maldonado <ema14mt@gmail.com>
2023-02-03 11:06:26 -0300

    Temporal theme created

---
+++ src/config/theme.config.tsx
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material/styles";
+
+export enum themePalette {
+  Dark01 = "#091625",
+  Dark03 = "#3A4451",
+  Dark04 = "#535C67",
+  Dark05 = "#6B737C",
+  Dark06 = "#848A92",
+  Dark09 = "#CED0D3",
+  Dark10 = "#E6E8E9",
+}
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: themePalette.Dark01,
+    },
+  },
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { theme } from "./config";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
