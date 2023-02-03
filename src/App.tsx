969cbaaeae4cedc18cca1f3c0c81aed2c4e08f88
Emanuel Maldonado <ema14mt@gmail.com>
2023-02-03 11:06:55 -0300

    CartPage created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { CartPage } from "./pages";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CartPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/CartPage/CartPage.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { Navbar } from "../../components";
+
+const CartPage: React.FC = () => {
+  return <Navbar />;
+};
+
+export default CartPage;
